1fefc807b9502e7c9ef0a4bab76ebbde78f894af
Rishabh Kumar <110474658+rishabhkr1@users.noreply.github.com>
2025-01-29 16:16:27 +0530

    one to many bidirectional

---
+++ 03-jpa-one-to-many/src/main/java/com/daytona/cruddemo/dao/AppDAO.java
@@ -0,0 +1,25 @@
+package com.daytona.cruddemo.dao;
+
+import com.daytona.cruddemo.entity.Course;
+import com.daytona.cruddemo.entity.Instructor;
+import com.daytona.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+    Instructor findInstructorById(int theId);
+    void deleteInstructorById(int theId);
+    InstructorDetail findInstructorDetailById(int theId);
+    void deleteInstructorDetailsById(int theId);
+    List<Course> findCoursesByInstructorId(int theId);
+    Instructor findInstructorByIdJoinFetch(int theId);
+    void update(Instructor tempInstructor);
+    void update(Course tempCourse);
+    Course findCourseById(int theId);
+    void deleteCourseById(int theId);
+
+
+
+
+}
